ec2f73bd56878de25d38a1736adada213900ec27
diallodeveloper <diallocodeur@gmail.com>
2023-03-27 11:03:33 +0000

    add requirements.txt file

--- djangocrm/settings.py
+++ djangocrm/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'crm_db',
-        'USER': 'diallodeveloper',
+        'USER': 'root',
         'PASSWORD': 'passer',
         'HOST': 'localhost',
         'PORT': '3306',
--- mydb.py
+++ mydb.py
@@ -1,9 +1,10 @@
 import mysql.connector
+# import psycopg2
 
 dataBase = mysql.connector.connect(
-    host = 'localhost',
-    user = 'diallodeveloper',
-    password = 'passer'
+    host='localhost',
+    user='root',
+    password='passer'
 )
 
 # prepare a cursor object
@@ -11,5 +12,14 @@ cursorObject = dataBase.cursor()
 
 cursorObject.execute("CREATE DATABASE crm_db")
 
-print("All Done!")
 
+# conn = psycopg2.connect("dbname=django_crm user=postgres")
+
+# cursorObject = conn.cursor()
+
+# cursorObject.execute("SELECT * FROM django_crm")
+
+# records = cursorObject.fetchall()
+
+
+print("All Done!")
